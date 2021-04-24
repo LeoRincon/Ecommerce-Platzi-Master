207a34158ebfb68de9e6ee1b4ee73d2b480204b7
Leonardo Rincón <jleobotache@gmail.com>
2021-04-24 15:15:46 -0500

    add the call to the api, and the painting of the objects in the sun. stylos adjustments to card-products and setting the logic to add the products to the cart

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,9 @@
 const iconMenu = document.getElementById("icon-menu");
 const menu = document.getElementById("menu");
-const mount = document.getElementById("mount");
+const mount = document.getElementById("mount-products");
 const templateProduct = document.getElementById("container-products").content;
 const fragment = document.createDocumentFragment();
+const Car = {};
 
 // ******************* Code Menu
 
@@ -13,7 +14,16 @@ const openCloseMenu = () => {
 
 iconMenu.addEventListener("click", openCloseMenu);
 
-// ******************* Code cal to the Api
+// ******************* Code call to the Api
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  console.log(e);
+  fetchData();
+});
+
+mount.addEventListener("click", (e) => {
+  addCar(e);
+});
 
 const fetchData = async () => {
   try {
@@ -23,12 +33,41 @@ const fetchData = async () => {
     // debugger;
     const data = await answer.json();
     // console.log(data);
-    addProducts(data);
+    paintProducts(data);
   } catch (error) {
     console.log(error);
   }
 };
 
-const addProducts = (data) => {
-  console.log(data);
+const paintProducts = (data) => {
+  // console.log(data);
+  data.forEach((product) => {
+    // console.log(product);
+    templateProduct.querySelector("img").setAttribute("src", product.img);
+    templateProduct.querySelector("h4").textContent = product.make;
+    templateProduct.querySelector("h5").textContent = product.model;
+    templateProduct.querySelector("span").textContent = product.price;
+    templateProduct.querySelector(".fovorite-car--car").dataset.id = product.id;
+
+    const clone = templateProduct.cloneNode(true);
+    fragment.appendChild(clone);
+  });
+  mount.appendChild(fragment);
+};
+
+const addCar = (e) => {
+  // console.log(e.target);
+  // console.log(e.target.classList.contains("fovorite-car--car"));
+  if (e.target.classList.contains("fovorite-car--car")) {
+    setCar(e.target.parentElement.parentElement);
+  }
+  e.stopPropagation();
+};
+
+const setCar = (obj) => {
+  // console.log(obj);
+  const product = {
+    id: obj.querySelector(".fovorite-car--car").dataset.id,
+  };
+  console.log(product);
 };
